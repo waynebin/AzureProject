0520f8252124a18d09a7b98e99fc55ef412ab5a9
waynestockin <77464752+waynestockin@users.noreply.github.com>
2024-07-07 23:13:10 -0400

    changes to the us notes

--- SQLQuery_1_population.sql
+++ SQLQuery_1_population.sql
@@ -1,22 +1,33 @@
-IF OBJECT_ID('StatePopulationComparisonView', 'V') IS NOT NULL
-   DROP VIEW StatePopulationComparisonView;
-GO
+SELECT 'Mean_Median_Household_Income' AS Statistic, AVG(Median_Household_income) AS Value FROM [dbo].[2022_income_Estimate] WHERE Median_Household_income >= 100
 
-CREATE VIEW StatePopulationComparisonView AS
-SELECT 
-    State_Postal_Code,
-    SUM([ChildrenInPoverty]) AS Total_Estimated_Youth_Poverty,
-    SUM(Estimated_Population_5_17) AS State_Children_Population
-FROM 
-    USSchoolDistrict
-GROUP BY 
-    State_Postal_Code;
-GO
+UNION ALL
 
-SELECT 
-    State_Postal_Code, 
-    Total_Estimated_Youth_Poverty,
-    State_Children_Population
-FROM 
-    StatePopulationComparisonView;
-GO
+SELECT 'StdDev_Median_Household_Income' AS Statistic, STDEV(Median_Household_income) AS Value FROM [dbo].[2022_income_Estimate] WHERE Median_Household_income >= 100
+
+UNION ALL
+
+SELECT 'Min_Median_Household_Income' AS Statistic, MIN(Median_Household_income) AS Value FROM [dbo].[2022_income_Estimate] WHERE Median_Household_income NOT IN (0, 1)
+
+UNION ALL
+
+SELECT 'Max_Median_Household_Income' AS Statistic, MAX(Median_Household_income) AS Value FROM [dbo].[2022_income_Estimate] WHERE Median_Household_income NOT IN (0, 1)
+
+UNION ALL
+
+SELECT 'Total_Poverty_Estimate_Age_5_17' AS Statistic, SUM(ChildrenInPoverty) AS Value FROM USSchoolDistrict WHERE ChildrenInPoverty >= 100
+
+UNION ALL
+
+SELECT 'Mean_Poverty_Estimate_Age_5_17' AS Statistic, AVG(ChildrenInPoverty) AS Value FROM USSchoolDistrict WHERE ChildrenInPoverty >= 100
+
+UNION ALL
+
+SELECT 'StdDev_Poverty_Estimate_Age_5_17' AS Statistic, STDEV(ChildrenInPoverty) AS Value FROM USSchoolDistrict WHERE ChildrenInPoverty >= 100
+
+UNION ALL
+
+SELECT 'Min_Poverty_Estimate_Age_5_17' AS Statistic, MIN(ChildrenInPoverty) AS Value FROM USSchoolDistrict WHERE ChildrenInPoverty NOT IN (0, 1)
+
+UNION ALL
+
+SELECT 'Max_Poverty_Estimate_Age_5_17' AS Statistic, MAX(ChildrenInPoverty) AS Value FROM USSchoolDistrict WHERE ChildrenInPoverty NOT IN (0, 1);
---
+++ SQLQuery_MedianIncome_povertyRate.sql
@@ -0,0 +1,27 @@
+-- Calculate the poverty rate and join with median household income
+WITH PovertyRates AS (
+    SELECT 
+        usd.State_Postal_Code,
+        usd.District_ID,
+        usd.School_District,
+        usd.Estimated_Population_5_17,
+        usd.ChildrenInPoverty,
+        (usd.ChildrenInPoverty * 1.0 / usd.Estimated_Population_5_17) * 100 AS PovertyRate,
+        inc.Median_Household_Income
+    FROM 
+        dbo.USSchoolDistrict usd
+    JOIN 
+        dbo.[2022_income_Estimate] inc
+    ON 
+        usd.State_Postal_Code = inc.Postal_Code
+    WHERE 
+        usd.Estimated_Population_5_17 > 0 -- To avoid division by zero
+)
+
+-- Select the relevant columns for the scatter plot
+SELECT 
+    Median_Household_Income,
+    PovertyRate,
+    State_Postal_Code
+FROM 
+    PovertyRates;
